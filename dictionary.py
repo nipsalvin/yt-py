c765f69affbfd8203d154d938c4c76820202fcca
nipsalvin <amwaniki.am@gmail.com>
2022-01-27 16:56:11 +0300

    initial commit

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+keys = [1, 2, 3, 4]
+values = ["Name", "Age", "Height", "Weight",]
+
+#Dict assigns values to keys
+assignment = dict(zip(keys,values))
+
+print (assignment)
+
+#adding values to assignment
+assignment[5] = "Location"
+
+print (assignment)
