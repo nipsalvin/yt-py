c2231b52829cd31f2f5fd8ddec1b191a7395c966
nipsalvin <amwaniki.am@gmail.com>
2022-02-01 16:36:09 +0300

    Getting Inputs

---
+++ input.py
@@ -0,0 +1,18 @@
+fname = input ("Enter First Name ")
+lname = input ("Enter Last Name ")
+
+name = fname + ' ' + lname
+print(name)
+#prints first character
+print(name[0])
+
+x = input("First Number ? ")
+int (x)
+y = input("Second Number ? ")
+int (y)
+z = y + x
+print (z)
+
+#prints evaluation
+result = eval(input("Enter marks? "))
+print(result)
