731ab8abe7f03854ca141b69837d7829c3fcf840
nipsalvin <amwaniki.am@gmail.com>
2022-02-01 15:55:07 +0300

    Math Functions

---
+++ math.py
@@ -0,0 +1,21 @@
+#importing math module
+import math
+#you can name how you want to import a module (line 20)
+import math as m
+
+x = math.sqrt(25)
+print (x)
+
+y = math.sqrt(2334)
+#prints the integer rounded off to the lower number
+print (math.floor(y))
+#prints the integer rounded off to the higher number
+print (math.ceil(y))
+
+#prints 3 to power 2
+print (math.pow(3,2))
+
+print (math.pi)
+
+a = m.sqrt(234)
+print (a)
