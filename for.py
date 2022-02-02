7d9a2544433648c10014b6923729d41cfdd4a652
nipsalvin <amwaniki.am@gmail.com>
2022-02-02 16:33:09 +0300

    for loop

---
+++ for.py
@@ -0,0 +1,16 @@
+x =  ["Nips", 99, 43.4]
+
+for i in x:
+    print (i)
+
+#Prints from 11 to 21 with increment of 2
+for j in range (1,100,2):
+    #Adds parameter to only print out numbers divisible by 5
+    if j % 5 == 0:
+        print (j)
+
+#Prints 1 letter at a time
+y = "Nips Alvin"
+
+for l in y:
+    print(l)
