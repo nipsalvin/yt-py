98adfd0c70b036cf35b47c822e73ffe9daf1d8aa
nipsalvin <amwaniki.am@gmail.com>
2022-02-02 16:02:51 +0300

    While loops

---
+++ while.py
@@ -0,0 +1,30 @@
+#Assign Value
+i = 1
+
+#Condition
+while i <= 5:
+    print("Nips")
+    #Increment
+    i = i + 1
+
+#In reverse
+j = 5
+
+while j >= 1:
+    #print with value of j
+    print ("Alvin", j)
+    j = j - 1
+
+x = 1
+
+while x <= 5:
+    #End commands that the program remains on the same line for the execution
+    print("Nips " ,end="")
+    
+    y = 1
+    while y <= 4:
+        print ("Greatness " ,end="")
+        y = y + 1
+    x = x + 1
+    #Tells the program to print a new line after every iteration
+    print()
