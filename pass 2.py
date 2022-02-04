73678cae1ab29394ed2ed1d6d8a71c51e8ae057b
nipsalvin <amwaniki.am@gmail.com>
2022-02-04 15:30:53 +0300

    break, continue & pass

---
+++ pass 2.py
@@ -0,0 +1,11 @@
+for i in range(5):
+
+    if i == 3:
+        #breaks loop after 3 iterations
+        break
+    print ("Hello Friend", i)
+
+def func():
+
+    #pass skips the block 
+    pass
--- pass.py
+++ pass.py
@@ -1,6 +1,8 @@
 for i in range(1,21):
-    #Passes numbers that are not even
-    if (i%2 != 0):
+    #Parses numbers that are not even
+    if i%2 != 0:
+    #This can also be expressed as
+    #if (i%2 != 0):
         pass
 
     else:
