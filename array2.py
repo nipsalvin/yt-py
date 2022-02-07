305a1ff8810aaca6e17371cd0d0641ec01077912
nipsalvin <amwaniki.am@gmail.com>
2022-02-07 16:38:04 +0300

    array from user and search

---
+++ array2.py
@@ -0,0 +1,27 @@
+#Getting input for array from user
+from array import *
+
+a = array("i",[])
+
+n = int(input("Enter the length of array "))
+
+for i in range(n):
+    x = int(input("Enter the next value "))
+    a.append(x)
+
+print (a)
+
+#Searching for indexes in array
+val = int(input("Enter value to be searched "))
+
+b = 0
+for c in a:
+    if c == val:
+        print(b)
+    
+    else:
+        print("Not found")
+        break
+
+    b += 1
+
