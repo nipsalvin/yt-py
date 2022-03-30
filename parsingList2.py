b9414dfd6aecf7040a956aa1e78d07e57bc94af6
nipsalvin <amwaniki.am@gmail.com>
2022-03-30 13:56:27 +0300

    passing List with input from users

--- parsingList2.py
+++ parsingList2.py
@@ -1,17 +1,19 @@
-list_names = []
-def func(list_names):
-    n = int(input("Enter the lenght of the list: "))
-    for i in range (1,n+1):
-        names = input("Enter the names: ")
-        list_names.append(names)
-    print("Here the list of names:", list_names)
-    more_five =[]
-    less_five = []
-    for k in list_names:
-        if len(k)>=5:
-            more_five.append(k)
-        else:
-            less_five.append(k)
-    print(f"five letters and above {more_five}\nless than five letters {less_five} ")
+name_list = [] #Create an emoty list
+def solution(): #Create function
+    n = int(input("Enter number here ")) #Create empty variable 'n' to get input(int) from user
+    for a in range(1,1+n): #Iterate through the loop until values in 'a' are the same number as '1+n' 
+        a = input("Enter name here ") #asks for input from user
+        name_list.append(a) #appends the empty list (name_list) with input from the user
+    more = [] #create empty list 'more'
+    less = [] #create empty list 'less'
 
-func(list_names)
+    for x in name_list: #iterates through the 'name_list' picking one value at a time
+        if len(x) >= 5: #checks if length of 'x' is greater or equal to 5
+            more.append(x) #if 'x' is greater than 5 then 'more' list is appended
+            print(x)
+        else:
+            less.append(x) #if 'x' is less than 5 then 'less' list is appended
+            print(x)
+    print("n is",n,"a is",a,"more is",more,"less is",less, "x is",x)
+    print(name_list)
+solution()
