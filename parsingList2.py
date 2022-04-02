b1a4f6596c6b0b69137af1763d02c7713d8b15b5
Sir Mwaniki Alvin IV <68748209+nipsalvin@users.noreply.github.com>
2022-04-02 12:29:36 +0300

    Better label

--- parsingList2.py
+++ parsingList2.py
@@ -1,7 +1,7 @@
 name_list = [] #Create an empty list
 def solution(): #Create function
     n = int(input("Enter number here ")) #Create empty variable 'n' to get input(int) from user
-    for a in range(1,1+n): #Iterate through the loop until values in 'a' are the same number as '1+n' 
+    for a in range(1,1+n): #Iterate through the loop until values in 'a' are the same number as '1+n' starting from 1st value
         a = input("Enter name here ") #asks for input from user
         name_list.append(a) #appends the empty list (name_list) with input from the user
     more = [] #create empty list 'more'
