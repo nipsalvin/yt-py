ea6ab5a5ae1ecefc14f6b53ee220474e0e33fbae
nipsalvin <amwaniki.am@gmail.com>
2022-04-06 15:56:14 +0300

    Better labels

--- numpy3.py
+++ numpy3.py
@@ -7,25 +7,25 @@ a = array([1,2,3,4,5.9],int)
 
 #prints type of values
 print(a.dtype)
-print(a)
+print("a is ",a)
 
 #Array using linspace()
 #0 is the start, 20 is the end 5 is the number of parts(Divisions)
 #it uses float type
 #if you use without parts it will print 20 parts
 b = linspace(0,20,5)
-print(b)
+print("b is ",b)
 #you can use [n] to specify the value to be printed
 print(b[3])
 
 #Array using logspace()
 c = logspace(1,40,5)
-print(c)
+print("c is ",c)
 
 #Array using zeros() and ones()
 #to convert to int we use ,int
 d = zeros(5)
 e = ones(5,int)
 
-print(d)
-print(e)
+print("d is ",d)
+print("e is ",e)
