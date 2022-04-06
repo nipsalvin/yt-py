22056ad8122d6337af465d7ddb5d88f58af7e29f
nipsalvin <amwaniki.am@gmail.com>
2022-04-06 17:13:05 +0300

    Module import

---
+++ modules/DemoModule.py
@@ -0,0 +1,7 @@
+from calc import *
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+c = add(a,b)
+print("C is: ",c)
---
+++ modules/calc.py
@@ -0,0 +1,13 @@
+
+def add(a,b):
+    return a+b
+
+def minus(a,b):
+    return a-b
+
+def divide(a,b):
+    return(a/b)
+
+def multiply(a,b):
+    return(a*b)
+    
