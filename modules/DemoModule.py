afa89ee4ec3365a7a61a7361606358c5000c80a2
nipsalvin <amwaniki.am@gmail.com>
2022-04-07 09:11:03 +0300

    Module import

--- modules/DemoModule.py
+++ modules/DemoModule.py
@@ -4,4 +4,13 @@ a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 
 c = add(a,b)
-print("C is: ",c)
+print("C is:",c)
+
+d = divide(a,b)
+print("d is:",d)
+
+e = multiply(a,b)
+print("e is:",e)
+
+f = minus(a,b)
+print("f is:",f)
