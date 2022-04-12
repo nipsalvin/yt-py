7299000f8ba955e1bbd16755cd6c93e335804f59
nipsalvin <amwaniki.am@gmail.com>
2022-04-12 11:54:55 +0300

    constructor

--- classes/constructor.py
+++ classes/constructor.py
@@ -1,34 +1,35 @@
+
 class People:
     
     def __init__(self):
-        self.age = 20
-        self.name = "Alvin"
+        self.age = 20 #First definition of age
+        self.name = "Alvin" #First definition of name
 
     def new(self):
-        self.age = 25
-        self.name = "Nips"
+        self.age = 25 #New definition of age
+        self.name = "Nips" #New definition of name
 
-    def compare(self, other):
-        if self.age == other.age:
+    def compare(self, other): #Function for comparison
+        if self.age == other.age: #Comparing if 2 'ages' are alike
             return True
         else:
             return False
 
-x = People()
-y = People()
+x = People() #Creating object from class People
+y = People() #Creating object from class People
 
-y.name = "PinPin"
-x.age = 30
+y.name = "PinPin" #New definition of name (y.name)
+x.age = 30 #New definition of age (x.age)
 
 print(x.name, x.age)
 print(y.name, y.age)
 
-x.new()
+x.new() #New definition of x using function "new"
 print(x.name, x.age)
 
 x1 = People()
 y1 = People()
-if x.compare(y):
+if x1.compare(y1): #Comparing 2 objects
     print("They are same")
 else:
     print("They are not the same")
