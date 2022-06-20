89b92eec9caa7fb99a969bb1322a4078d3473751
nipsalvin <amwaniki.am@gmail.com>
2022-06-20 12:26:00 +0300

    pytest added

--- testfile.py
+++ testfile.py
@@ -1,28 +1,20 @@
-class Student:
+class PyCharm:
+    def func(self): #you must have this method for "func" for all objects
+        print("Compiling")
+        print("Running")
 
-    def __init__(self, name, rollno ):
-        self.name = name
-        self.rollno = rollno
-        self.lap = self.Laptop()
-    
-    def show(self):
-        print(self.name, self.rollno)
-        self.lap.show()
+class VsCode:
+    def func(self): #you must have this method for "func" for all objects
+        print("Spell check")
+        print("syntax check")
+        print("Compiling")
+        print("Running")
+class Laptop:
+    def code(self, ide):
+        ide.func()
 
-    class Laptop: #Creating an inner class
-        def __init__(self):
-            self.brand = "HP\n"
-            self.cpu = "Core i5\n"
-            self.ram = 6
-        
-        def show(self):
-            print(self.brand, self.cpu, self.ram)
+ide = PyCharm() #you can choose an IDE here
 
-s1 = Student("Alvin", 2) #creating an object of outter class by calling it
-s2 = Student("Pinpin", 3)
-s2.show()
-
-lap1 = Student.Laptop()#Creating an object (lap1) of inner class(Laptop) by calling it through the outter class (Student)
-lap1.show()
-lap2 = s1.lap
-lap2.show()
+x = Laptop()
+x.code(ide)
+#x.code(VsCode)
