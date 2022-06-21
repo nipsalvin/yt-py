25e58cb007b0dc7b91941c0c984ec8718b3ec5d6
nipsalvin <amwaniki.am@gmail.com>
2022-06-21 13:02:04 +0300

    polymorphism - Duck Typing

--- polymorphism_DuckTyping.py
+++ polymorphism_DuckTyping.py
@@ -6,14 +6,14 @@ class PyCharm:
 class VsCode:
     def func(self): #you must have this method for "func" for all objects
         print("Spell check")
-        print("syntax check")
+        print("syntax check") 
         print("Compiling")
         print("Running")
 class Laptop:
-    def code(self, ide):
-        ide.func()
+    def code(self, ide): #you must have this method for "code" for all objects
+        ide.func() #you can use the object of the class ide to call the method func of the class ide (ide is the object of the class ide) and it will call the method func of the class ide and it will print the result of the method func of the class ide
 
-ide = PyCharm() #you can choose an IDE here
+ide = PyCharm() #you can choose an IDE here (ide is the object of the class IDE) and it will call the method func of the class IDE and it will print the result of the method func of the class IDE (IDE is the class IDE) 
 
 x = Laptop()
 x.code(ide)
