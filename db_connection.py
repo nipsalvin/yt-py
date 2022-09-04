25a81133907cb1d9fa630287e7eebae3f8a24968
nipsalvin <amwaniki.am@gmail.com>
2022-09-04 14:52:55 +0300

    Comments

--- db_connection.py
+++ db_connection.py
@@ -1,10 +1,12 @@
+#Make sure you have mysql installed
+#Make sure MySql service is running
 import mysql.connector
 
-mydb = mysql.connector.connect(host= "localhost", user="root", passwd="", database = "nips")
+mydb = mysql.connector.connect(host= "localhost", user="root", passwd="", database = "nips") #Specify database and credentials here
 
 mycursor = mydb.cursor()
 
-mycursor.execute("select * from students")
+mycursor.execute("select * from students") #enter mysql query here
 
 for i in mycursor:
     print (i)
